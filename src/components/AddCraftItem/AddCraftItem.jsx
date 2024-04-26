20e9e3c750fc857a5c353f089093b7913baf5bde
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-04-26 18:19:15 +0600

    Adding craft item page

--- src/components/AddCraftItem/AddCraftItem.jsx
+++ src/components/AddCraftItem/AddCraftItem.jsx
@@ -1,7 +1,172 @@
+import Swal from "sweetalert2";
+
 const AddCraftItem = () => {
+  const handleAddCraft = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
+
+    const newCoffee = {
+      name,
+      quantity,
+      supplier,
+      taste,
+      category,
+      details,
+      photo,
+    };
+
+    console.log(newCoffee);
+  };
+
   return (
-    <div>
-      <h2>User will add craft items here</h2>
+    <div className="bg-[#F4F3F0] text-black p-24">
+      <h2 className="text-3xl font-extrabold text-pink-600 text-center">Add A Craft Item</h2>
+      <form onSubmit={handleAddCraft}>
+        {/* Form Name and Quantity Row */}
+        <div className="md:flex gap-4 mt-2">
+          <div className="md:w-1/2">
+            <label
+              htmlFor="url"
+              className="block mb-1 text-sm font-medium text-black"
+            >
+              Coffee Name
+            </label>
+            <div className="flex">
+              <input
+                type="text"
+                name="name"
+                id=""
+                placeholder="Coffee Name"
+                className="flex py-2 w-full flex-1 border sm:text-sm rounded-r-md focus:ring-inset border-gray-300 px-2 text-black bg-white focus:ring-violet-400"
+              />
+            </div>
+          </div>
+          <div className="md:w-1/2">
+            <label
+              htmlFor="url"
+              className="block mb-1 text-sm font-medium text-black"
+            >
+              Available Quantity
+            </label>
+            <div className="flex">
+              <input
+                type="text"
+                name="quantity"
+                id=""
+                placeholder="Available Quantity"
+                className="flex py-2 flex-1 border sm:text-sm rounded-r-md focus:ring-inset border-gray-300 px-2 text-black bg-white focus:ring-violet-400"
+              />
+            </div>
+          </div>
+        </div>
+        {/* Form Supplier and Taste Row */}
+        <div className="md:flex gap-4 mt-2">
+          <div className="md:w-1/2">
+            <label
+              htmlFor="url"
+              className="block mb-1 text-sm font-medium text-black"
+            >
+              Supplier Name
+            </label>
+            <div className="flex">
+              <input
+                type="text"
+                name="supplier"
+                id=""
+                placeholder="Supplier Name"
+                className="flex py-2 w-full flex-1 border sm:text-sm rounded-r-md focus:ring-inset border-gray-300 px-2 text-black bg-white focus:ring-violet-400"
+              />
+            </div>
+          </div>
+          <div className="md:w-1/2">
+            <label
+              htmlFor="url"
+              className="block mb-1 text-sm font-medium text-black"
+            >
+              Taste
+            </label>
+            <div className="flex">
+              <input
+                type="text"
+                name="taste"
+                id=""
+                placeholder="Taste"
+                className="flex py-2 flex-1 border sm:text-sm rounded-r-md focus:ring-inset border-gray-300 px-2 text-black bg-white focus:ring-violet-400"
+              />
+            </div>
+          </div>
+        </div>
+        {/* Form Category and Details Row */}
+        <div className="md:flex gap-4 mt-2">
+          <div className="md:w-1/2">
+            <label
+              htmlFor="url"
+              className="block mb-1 text-sm font-medium text-black"
+            >
+              Category
+            </label>
+            <div className="flex">
+              <input
+                type="text"
+                name="category"
+                id=""
+                placeholder="Category"
+                className="flex py-2 w-full flex-1 border sm:text-sm rounded-r-md focus:ring-inset border-gray-300 px-2 text-black bg-white focus:ring-violet-400"
+              />
+            </div>
+          </div>
+          <div className="md:w-1/2">
+            <label
+              htmlFor="url"
+              className="block mb-1 text-sm font-medium text-black"
+            >
+              Details
+            </label>
+            <div className="flex">
+              <input
+                type="text"
+                name="details"
+                id=""
+                placeholder="Details"
+                className="flex py-2 flex-1 border sm:text-sm rounded-r-md focus:ring-inset border-gray-300 px-2 text-black bg-white focus:ring-violet-400"
+              />
+            </div>
+          </div>
+        </div>
+        {/* Form Photo Row */}
+        <div className="mt-2">
+          <div className="md:w-full">
+            <label
+              htmlFor="url"
+              className="block mb-1 text-sm font-medium text-black"
+            >
+              Photo
+            </label>
+            <div className="flex">
+              <input
+                type="text"
+                name="photo"
+                id=""
+                placeholder="Photo"
+                className="flex py-2 w-full flex-1 border sm:text-sm rounded-r-md focus:ring-inset border-gray-300 px-2 text-black bg-white focus:ring-violet-400"
+              />
+            </div>
+          </div>
+        </div>
+        <input
+          type="submit"
+          value="Add Coffee"
+          className="btn mt-4 bg-black text-white btn-block"
+        />
+      </form>
     </div>
   );
 };
